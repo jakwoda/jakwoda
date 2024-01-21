94182205c82d5ee156da5b568896b34402bcbb12
Mate <matmieciek@gmai.com>
2024-01-21 20:56:22 +0100

    change links

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ modal.style.display='block';
 // buttonAbout.addEventListener("enter", showPicture)
 
 buttonAbout.addEventListener("click", showModal)
+
+function copyClipboard() {
+  // Get the text fielda
+ let copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
